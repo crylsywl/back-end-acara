743004b393ca84af05d60c02de7b112c07f48a1e
crylsywl <crylsyawal123@gmail.com>
2025-04-04 20:16:39 +0700

    Save Data Register

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
 import express from "express";
-import routes from "./routes/api"
+import routes from "./routes/api";
+import bodyParser from "body-parser";
+import db from "./utils/database";
 
-const app = express();
+async function init() {
+  try {
+    const result = await db();
 
-const PORT = 3000;
+    console.log("database Status: ", result);
 
-app.use("/api", routes);
+    const app = express();
 
+    app.use(bodyParser.json());
 
-app.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`);
-});
+    const PORT = 3000;
+
+    app.use("/api", routes);
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+init();
