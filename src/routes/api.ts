d42f1b79b1ac63be81ad0a670fca00c26f0e7194
crylsywl <crylsyawal123@gmail.com>
2025-04-04 18:53:45 +0700

    update

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import dummyController from "../controllers/dummy.controllers";
+import authController from "../controllers/auth.controller";
+
 
 const router = express.Router();
 
-router.get("/dummy", dummyController.dummy);
+router.post("/auth/register", authController.register );
 
 export default router;
